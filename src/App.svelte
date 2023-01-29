ef19dcea7683c1c29faef0425e141e6e7dbafab7
Ty Geri <tygeri@college.harvard.edu>
2023-01-29 13:46:25 -0500

    Updated comments

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,22 @@
 <script>
   import { afterUpdate } from 'svelte';
 
+  // This is an example of a 'Reactive Value' where the DOM is updated
   afterUpdate(() => {
     document.querySelector('.js-todo-input').focus();
   });
   
+  // This is an example of 'Variables'
   let todoItems = [];
+  // This is an example of 'Variables'
   let newTodo = '';
   
   function addTodo() {
     newTodo = newTodo.trim();
+    // This is an example of 'Control Flow', a conditional
     if (!newTodo) return;
-  
+    
+    // This is an example of 'Props', properties of an object
     const todo = {
         text: newTodo,
         checked: false,
@@ -34,9 +39,10 @@
   <!-- The classes are examples of 'Reusable Components' -->
   <div class="container">
     <h1 class="app-title">todos</h1>
-    <!-- This is an example of 'Control Flow' - a loop through items with a conditional check -->
+    <!-- This is an example of 'Control Flow' - a loop through items -->
     <ul class="todo-list">
       {#each todoItems as todo (todo.id)}
+       <!-- This is an example of 'Control Flow' - a conditional check -->
         <li class="todo-item {todo.checked ? 'done' : ''}">
           <input id={todo.id} type="checkbox" />
           <label for={todo.id} class="tick" on:click={() => toggleDone(todo.id)}></label>
@@ -55,6 +61,7 @@
     </div>
     <!-- This is an example of a 'Reactive Value' where the DOM is updated -->
     <form on:submit|preventDefault={addTodo}>
+      <!-- This is an example of a 'Binding' -->
       <input class="js-todo-input" type="text" aria-label="Enter a new todo item" placeholder="E.g. Build a web app" bind:value={newTodo}>
     </form>
   </div>
