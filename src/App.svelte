64dfe30fc7f60c4a28265c94f77ff85f12e1d850
Ty Geri <tygeri@college.harvard.edu>
2023-01-29 13:37:16 -0500

    Updated to create funcitoning ToDo app

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,61 @@
 <script>
-  // component logic
-</script>
+  import { afterUpdate } from 'svelte';
 
-<!-- component markup -->
+  afterUpdate(() => {
+    document.querySelector('.js-todo-input').focus();
+  });
+  
+  let todoItems = [];
+  let newTodo = '';
+  
+  function addTodo() {
+    newTodo = newTodo.trim();
+    if (!newTodo) return;
+  
+    const todo = {
+        text: newTodo,
+        checked: false,
+        id: Date.now(),
+    };
+  
+    todoItems = [...todoItems, todo];
+    newTodo = '';
+  }
+  function toggleDone(id) {
+    const index = todoItems.findIndex(item => item.id === Number(id));
+    todoItems[index].checked = !todoItems[index].checked;
+  }
+  function deleteTodo(id) {
+    todoItems = todoItems.filter(item => item.id !== Number(id));
+  }
+  </script>
+
+<main>
+  <!-- The classes are examples of 'Reusable Components' -->
+  <div class="container">
+    <h1 class="app-title">todos</h1>
+    <!-- This is an example of 'Control Flow' - a loop through items with a conditional check -->
+    <ul class="todo-list">
+      {#each todoItems as todo (todo.id)}
+        <li class="todo-item {todo.checked ? 'done' : ''}">
+          <input id={todo.id} type="checkbox" />
+          <label for={todo.id} class="tick" on:click={() => toggleDone(todo.id)}></label>
+          <span>{todo.text}</span>
+          <!-- This is an example of a 'Reactive Value' where the DOM is updated -->
+          <button class="delete-todo" on:click={() => deleteTodo(todo.id)}>
+            <svg><use href="#delete-icon"></use></svg>
+          </button>
+        </li>
+      {/each}
+    </ul>
+    <div class="empty-state">
+      <svg class="checklist-icon"><use href="#checklist-icon"></use></svg>
+      <h2 class="empty-state__title">Add your first todo</h2>
+      <p class="empty-state__description">What do you want to get done today?</p>
+    </div>
+    <!-- This is an example of a 'Reactive Value' where the DOM is updated -->
+    <form on:submit|preventDefault={addTodo}>
+      <input class="js-todo-input" type="text" aria-label="Enter a new todo item" placeholder="E.g. Build a web app" bind:value={newTodo}>
+    </form>
+  </div>
+</main>
